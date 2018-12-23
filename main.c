8c4fdf3a34b44b6813efd31a231e753203b30ee9
Thiago <thiago.adams@gmail.com>
2018-12-23 00:22:34 -0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//To debug linux
+//Run WSL
+//sudo service ssh start
+//More details at https://www.hanselman.com/blog/WritingAndDebuggingLinuxCApplicationsFromVisualStudioUsingTheWindowsSubsystemForLinux.aspx
+
+int main()
+{
+    printf("hello from WindowsLinux!\n");
+    return 0;
+}
